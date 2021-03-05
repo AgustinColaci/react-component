13b23359e74fcd6c9bd3769ed7630efd160629c7
Agustín Colaci <agustincolaci@gmail.com>
2021-03-05 14:58:50 -0300

    Desafío clase 3

---
+++ src/components/NavBar.js
@@ -0,0 +1,17 @@
+function NavBar() {
+  return (
+    <div className="App">
+      <nav>
+          <ul>
+              <li><a href="#index">Inicio</a></li>
+              <li><a href="#nosotros">Nosotros</a></li>
+              <li><a href="#servicios">Servicios</a></li>
+              <li><a href="#productos">Productos</a></li>
+              <li><a href="#contacto">Contacto</a></li>
+          </ul>
+      </nav>
+    </div>
+  );
+}
+
+export default NavBar;
